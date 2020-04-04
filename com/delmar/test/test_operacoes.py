02fcc40fd826160f0dd00eca02acd18c46ba7436
delmarhirata <delmar.hirata@aluno.faculdadeimpacta.com.br>
2020-04-04 16:17:03 -0300

    Travis

--- com/delmar/test/test_operacoes.py
+++ com/delmar/test/test_operacoes.py
@@ -5,6 +5,6 @@ class TestOperacoes(TestCase):
 
     def setUp(self):
         self.operacoes = Operacoes()
-
+    
     def test_soma(self):
-        self.assertEqual(self.operacoes.soma([1,5]), 6, "Shoud be 6")
+        self.assertEqual(self.operacoes.soma([7, 2]), 9, "Deveria ser 9")
